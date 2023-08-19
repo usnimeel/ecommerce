b5fb103d00410e1e7f9eed77a2c2d0607b7cf866
Minsu Lee <usnimeel@gmail.com>
2023-08-19 01:26:37 -0300

    Add: styles and scss

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./categories.styles.scss"
+
 const App = () => {
 
   const categories = [
@@ -25,11 +27,11 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map((category) => (
-          <div className="category-container">
+      {categories.map(({ id, title }) => (
+          <div key={id} className="category-container">
           <div className="background-image"/>
           <div className="category-body-container">
-            <h2>{category.title}</h2>
+            <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
         </div>          
